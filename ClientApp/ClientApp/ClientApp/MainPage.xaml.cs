273e9a832103b0b5ff5f2e2712c859acd529d0a1
SANA BEN <sana.bengannoune@gmail.com>
2021-06-14 21:39:02 +0100

    add api

--- ClientApp/ClientApp/ClientApp/MainPage.xaml.cs
+++ ClientApp/ClientApp/ClientApp/MainPage.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -10,9 +12,19 @@ namespace ClientApp
 {
     public partial class MainPage : ContentPage
     {
+        private string Api = "http://karangue.azurewebsites.net/api/apiDeclarations";
         public MainPage()
         {
             InitializeComponent();
+            Demarreur();
+        }
+
+        private async Task Demarreur()
+        {
+            var client = new HttpClient();
+            string json = await client.GetStringAsync(Api);
+            var products = JsonConvert.DeserializeObject<List<Product>>(json);
+            ListData.ItemsSource = products;
         }
     }
 }
---
+++ ClientApp/ClientApp/ClientApp/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClientApp
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+        public int Quantity { get; set; }
+        public string Image { get; set; }
+
+
+
+
+
+    }
+}
